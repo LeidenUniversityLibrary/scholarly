cc28b949c2fc39f076e500d7f9fcf62d954ba158
lucasvanschaik <schaik@library.leidenuniv.nl>
2019-05-24 15:59:18 +0200

    Add department in search result

--- templates/islandora-solr.tpl.php
+++ templates/islandora-solr.tpl.php
@@ -33,6 +33,10 @@
                 <?php endif; ?>
               <?php elseif ($key === 'related_mods_originInfo_encoding_w3cdtf_type_embargo_dateOther_mdt'): ?>
                 <dd style="display:none;"><?php print trim($value['value'], " \t\n\r"); ?></dd>
+              <?php elseif ($key === 'mods_genre_authority_local_s'): ?>
+                <dd class="solr-value <?php print $value['class']; ?>"><?php print trim($value['value'], " \t\n\r"); print ' | '; print trim($result['solr_doc']['mods_name_personal_affiliation_department_ms']['value'], " \t\n\r"); ?></dd>
+              <?php elseif ($key === 'mods_name_personal_affiliation_department_ms'): ?>
+                <?php // do not display the department here, did it above already ?>
               <?php else: ?>
                 <dd class="solr-value <?php print $value['class']; ?>"><?php print trim($value['value'], " \t\n\r"); ?></dd>
               <?php endif; ?>
