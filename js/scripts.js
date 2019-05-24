38afcc9c1adb9f349c95f8f08012db9fb1d9c821
lucasvanschaik <schaik@library.leidenuniv.nl>
2019-05-24 14:38:37 +0200

    Repair menu

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
   Drupal.behaviors.myModuleBehavior = {
     attach: function (context, settings) {
       $(document).ready(function() {
-        $('a.cbp-placeholder').next().slideToggle();
+        //$('a.cbp-placeholder').next().slideToggle();
         $('a.cbp-placeholder').click(function(event) {
           event.preventDefault();
           $(this).next().css('height', 'auto').slideToggle();
@@ -14,15 +14,18 @@
 
           //$(this).prev();
           // open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
-          $(this).slideToggle(500, function () {
+          //$(this).slideToggle(500, function () {
             // execute this after slideToggle is done
             // change text of header based on visibility of content div
 
             // change text based on condition
-            $(this).not(":visible") ?
-                $(this).addClass("hidden").removeClass("visible") :
-                $(this).addClass("visible").removeClass("hidden");
-          });
+            if ($(this).is(":visible")) {
+              $(this).addClass("visible").removeClass("hidden");
+            }
+            else {
+              $(this).addClass("hidden").removeClass("visible");
+            }
+          //});
         });
 
 
